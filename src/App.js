d1dc87ac3f3c5331d01460d36a103faff99de2eb
sbrij001 <sbrij001@yahoo.com>
2019-08-15 12:34:02 -0400

    adding styling to monsters rolodex. gradient and font change

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ class App extends Component {
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value})
+  }
   render(){
     // const monsters = this.state.monsters
     // const searchField = this.state.searchField
@@ -26,9 +30,10 @@ class App extends Component {
 
     return (
       <div>
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={ e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}/>
       </div>
